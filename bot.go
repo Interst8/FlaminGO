e84bb5fa5bf0493b7e902ea5122f09399f8730cb
Caleb Munn <clm1249@g.rit.edu>
2022-06-17 19:30:14 -0400

    Added getRecentObs function, seems to work

--- bot.go
+++ bot.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"log"
+	"net/http"
 	"os"
 
 	"github.com/bwmarrin/discordgo"
@@ -15,6 +18,24 @@ var (
 	Key   string
 	BotID string
 )
+
+var LCode = "L976278" //EBird Location code for Rochester Institute of Technology. Hardcoded for RIT Birding club, I plan to make this overridable by command
+var KM = 10           //Default size to search around location, 5 kilometers.
+var Lat = 43.08       //Latitude of RIT
+var Long = -77.67     //Longitude of RIT
+var RIT Location
+
+type Location struct {
+	code string
+	lat  float64
+	long float64
+	name string
+}
+
+type Bird struct {
+	ComName string
+	HowMany int
+}
 
 func init() {
 	// flag.StringVar(&Token, "t", "", "Bot Token")
@@ -31,10 +52,15 @@ func init() {
 	//Get Ebird API Key
 	Key = os.Getenv("EBIRD_KEY")
 
+	//Create RIT Location
+	RIT.code = LCode
+	RIT.lat = Lat
+	RIT.long = Long
+	RIT.name = "Rochester Institute of Technology"
+
 }
 
 func Start() {
-
 	//creating new bot session
 	goBot, err := discordgo.New("Bot " + Token)
 
@@ -66,14 +92,56 @@ func Start() {
 	fmt.Println("Bot is running !")
 }
 
-//Definition of messageHandler function it takes two arguments first one is discordgo.Session which is s , second one is discordgo.MessageCreate which is m.
+//Definition of messageHandler function it takes two arguments first one is discordgo.Session which is s, second one is discordgo.MessageCreate which is m.
 func messageHandler(s *discordgo.Session, m *discordgo.MessageCreate) {
 	//Bot musn't reply to it's own messages , to confirm it we perform this check.
 	if m.Author.ID == BotID {
 		return
 	}
 	//If we message ping to our bot in our discord it will return us pong .
-	if m.Content == "!ping" {
-		_, _ = s.ChannelMessageSend(m.ChannelID, "pong")
+	if m.Content == "!get" {
+		_, _ = s.ChannelMessageSend(m.ChannelID, getRecentObs(RIT, KM))
 	}
+}
+
+func getRecentObs(loc Location, radius int) string { //Gets a list of nearby observations in the specified radius (km) from a location
+	url := fmt.Sprintf("https://api.ebird.org/v2/data/obs/geo/recent?lat=%v&lng=%v&sort=species&dist=%d", loc.lat, loc.long, radius)
+	method := "GET"
+
+	client := &http.Client{}
+	req, err := http.NewRequest(method, url, nil)
+	if err != nil {
+		fmt.Println(err)
+		return err.Error()
+	}
+
+	req.Header.Add("X-eBirdApiToken", Key)
+
+	res, err := client.Do(req)
+	if err != nil {
+		fmt.Println(err)
+		return err.Error()
+	}
+	defer res.Body.Close()
+
+	body, err := ioutil.ReadAll(res.Body) //body is a JSON response
+	if err != nil {
+		fmt.Println(err)
+		return err.Error()
+	}
+
+	var b []Bird
+	err = json.Unmarshal(body, &b)
+	if err != nil {
+		fmt.Println(err)
+		return err.Error()
+	}
+
+	//Formatting return string
+	rString := fmt.Sprintf("Number of birds seen within %d km of %v:\n", radius, loc.name)
+	for i := 0; i < len(b); i++ {
+		rString += fmt.Sprintf("%v: %d\n", b[i].ComName, b[i].HowMany)
+	}
+
+	return rString
 }
